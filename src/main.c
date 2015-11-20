5e24fa0dce84ccda498f37642c33345e03cd6ede
42-Ceasure <m-one_laz@hotmail.fr>
2015-11-20 19:49:39 +0100

    ac le swagg

--- src/main.c
+++ src/main.c
@@ -22,10 +22,10 @@ int main(int ac, char **av, char **ep)
 	t_env	*e;
 	char 	*buf;
 
-	(void)av;
+	// (void)av;
 	if (ac == 1)
 	{
-		// ft_swagg(av[0]);
+		ft_swagg(av[0]);
 		e = (t_env *)malloc(sizeof(t_env));
 		parse_env(e, ep);
 		prompt(e);
