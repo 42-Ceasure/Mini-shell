acbf0222b645a4f5565502317b19ae488e8b8196
42-Ceasure <cesar.glavieux@live.fr>
2015-12-02 19:18:55 -0800

    e

--- src/main.c
+++ src/main.c
@@ -5,32 +5,55 @@ void ft_swagg(char *s)
 {
 		ft_putstr("launching ");
 		ft_putendl(s);
-		usleep(500000);
+		// usleep(500000);
+		ft_putstr("\e[1m\e[38;5;166m");
 		ft_putendl(";-.               ,");
-		usleep(500000);
+		// usleep(500000);
 		ft_putendl(" \\ '.           .'/");
-		usleep(500000);
+		// usleep(500000);
 		ft_putendl("  \\  \\ .---. .-' /");
-		usleep(500000);
+		// usleep(500000);
 		ft_putendl("   '. '     `\\_.'");
-		usleep(500000);
-		ft_putendl("     |(),()  |     ,");
-		usleep(500000);
-		ft_putendl("     (  __   /   .' \\");
-		usleep(500000);	
-		ft_putendl("    .''.___.'--,/\\_,|");
-		usleep(500000);
-		ft_putendl("   {  /     \\   }   |");
-		usleep(500000);
-		ft_putendl("    '.\\     /_.'    /");
-		usleep(500000);
-		ft_putendl("     |'-.-',  `; _.'");
-		usleep(500000);
-		ft_putendl("     |  |  |   |`");
-		usleep(500000);
-		ft_putendl("     `\"\"`\"\"`\"\"\"`");
-		usleep(500000);
-		ft_putchar('\n');
+		// usleep(500000);
+		ft_putstr("     |(),()  |     ");
+		ft_putstr("\e[1m\e[38;5;186m");
+		ft_putendl(",");
+		ft_putstr("\e[1m\e[38;5;166m");
+		// usleep(500000);
+		ft_putstr("     (  __   /   ");
+		ft_putstr("\e[1m\e[38;5;186m");
+		ft_putendl(".' \\");
+		// usleep(500000);	
+		ft_putstr("    .'");
+		ft_putstr("\e[1m\e[38;5;166m");
+		ft_putstr("'.___.'");
+		ft_putstr("\e[1m\e[38;5;186m");
+		ft_putstr("--,");
+		ft_putstr("\e[1m\e[38;5;166m");
+		ft_putstr("/");
+		ft_putstr("\e[1m\e[38;5;186m");
+		ft_putendl("\\_,|");
+		// usleep(500000);
+		ft_putstr("   {  /     \\   }   ");
+		ft_putstr("\e[1m\e[38;5;166m");
+		ft_putendl("|");
+		// usleep(500000);
+		ft_putstr("\e[1m\e[38;5;186m");
+		ft_putstr("    '.\\     /_.'    ");
+		ft_putstr("\e[1m\e[38;5;166m");
+		ft_putendl("/");
+		// usleep(500000);
+		ft_putstr("      \\");
+		ft_putstr("\e[1m\e[38;5;186m");
+		ft_putstr("'-.-'");
+		ft_putstr("\e[1m\e[38;5;166m");
+		ft_putendl("/ `; _.'");
+		// usleep(500000);
+		ft_putendl("       | | |  /");
+		// usleep(500000);
+		ft_putendl("      `\"\"'\"`\"\"`");
+		// usleep(500000);
+		ft_putendl("\e[0m");
 }
 
 void handler(int sig)
@@ -44,10 +67,10 @@ int main(int ac, char **av, char **ep)
 	t_env	*e;
 	char 	*buf;
 
-	signal(SIGINT, handler);
 	buf = NULL;
 	if (ac == 1)
 	{
+		signal(SIGINT, handler);
 		ft_swagg(av[0]);
 		e = (t_env *)ft_memalloc(sizeof(t_env));
 		parse_env(e, ep);
